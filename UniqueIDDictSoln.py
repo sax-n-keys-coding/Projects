b5bd4d03047cadb6b007cff9132c0b13039e9399
sax-n-keys-coding <54819707+sax-n-keys-coding@users.noreply.github.com>
2020-01-23 01:36:40 -0800

    Update UniqueIDDictSoln.py

--- UniqueIDDictSoln.py
+++ UniqueIDDictSoln.py
@@ -1,4 +1,21 @@
-def uniqueNum(lst):
+"""
+PROBLEM OVERVIEW:
+
+Each breakfast delivery is assigned a unique ID, a positive integer. 
+When one of the company's 100 drones takes off with a delivery, 
+the delivery's ID is added to an array, deliveryIdConfirmations. 
+When the drone comes back and lands, the ID is again added to the same array.
+
+After breakfast this morning there were only 99 drones on the tarmac. One of the drones never made it back from a delivery. 
+We suspect a secret agent from Amazon placed an order and stole one of our patented drones. 
+To track them down, we need to find their delivery ID.
+
+Given the array of IDs, which contains many duplicate integers and one unique integer, find the unique integer.
+
+Problem source: https://www.interviewcake.com/question/java/find-unique-int-among-duplicates
+"""
+
+def uniqueID(lst): #time complexity (I think): O(n)
     dictIds = {}
     for idNum in lst:
         if idNum in dictIds:
@@ -7,8 +24,14 @@ def uniqueNum(lst):
             dictIds.update({idNum:1})
     for idNum in dictIds.keys():
         tmp = dictIds[idNum]
-        if tmp == 1:
+        if tmp % 2 == 1:
             return idNum
     return "There is no unique key in list"
     
-uniqueNum([1123123,92391230,92391230,1123123,12391239,1234567,1234567])
+uniqueID([1123123,92391230,12391239, 92391230,1123123,12391239, 12391239, 1234567,1234567,12391239, 12391239]) 
+
+#Correction #1:
+#missing drone could have instances that are greater than 1, in fact:
+#the number of indices for the missing drone (uniqueID) will always be odd 
+
+#(a drone could take off and land some times and then get lost, thus making the drone ID's occurences odd)
